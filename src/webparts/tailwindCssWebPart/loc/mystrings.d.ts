14c06ab04df161491c91e35f603b96d6fe3b897c
Franck Cornu <franck.cornu@ubisoft.com>
2024-03-27 10:47:47 -0400

    - Initial commit project structure

---
+++ src/webparts/tailwindCssWebPart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITailwindCssWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TailwindCssWebPartWebPartStrings' {
+  const strings: ITailwindCssWebPartWebPartStrings;
+  export = strings;
+}
